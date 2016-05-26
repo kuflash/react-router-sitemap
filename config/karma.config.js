f18be732ffebb291e11e64972851053446839006
Igor Uvarov <kuflash@ya.ru>
2016-05-26 23:12:46 +0300

    fix(tests): fix karma config for CI

--- config/karma.config.js
+++ config/karma.config.js
@@ -47,6 +47,10 @@ module.exports = function (config) {
 		// enable / disable watching file and executing tests whenever any file changes
 		autoWatch: true,
 
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: true,
+
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
 		browsers: ['PhantomJS2'],
