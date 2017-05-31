4bb79b0128df80bb74b27620e895c6387049335e
Mats de Swart <mats@fovero.nl>
2017-05-31 14:59:32 +0200

    #49 Sitemap Index and Splitting into multiple sitemaps

--- example/sitemap-builder.js
+++ example/sitemap-builder.js
@@ -11,17 +11,42 @@ const filterConfig = {
 	],
 };
 
+
+// Setup some random projects
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
+const lotOfNames = [];
+
+function produceItem(arr, char) {
+	return arr.map(c => { lotOfNames.push(`${c}-${char}`) });
+}
+
+alphabet.map(c => produceItem(alphabet, c));
+
+const projects = {
+	projectName: lotOfNames,
+	achievement: lotOfNames
+};
+
+
 const paramsConfig = {
 	'/projects/:projectName': [
 		{ projectName: 'hello-world' },
 		{ projectName: 'second-project' },
 		{ projectName: ['third-project', 'fourth-project'] },
+		projects
 	],
 	'/projects/:projectName/view': [
 		{ projectName: 'hello-world' },
 		{ projectName: 'second-project' },
 		{ projectName: ['third-project', 'fourth-project'] },
+		projects
 	],
+	'/projects/:projectName/achievements/:achievement': [
+		{ projectName: 'hello-world' },
+		{ projectName: 'second-project' },
+		{ projectName: ['third-project', 'fourth-project'] },
+		projects
+	]
 };
 
 (
@@ -29,5 +54,5 @@ const paramsConfig = {
 		.filterPaths(filterConfig)
 		.applyParams(paramsConfig)
 		.build('http://my-site.ru')
-		.save('./sitemap.xml')
+		.save('./sitemap.xml', '/static/')
 );
