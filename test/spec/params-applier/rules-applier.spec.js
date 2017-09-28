9430c88c39eb557aa5a193cbb324d83779a4fc52
Francesco Rosso <frosso@spydertrap.com>
2017-09-28 15:42:43 -0500

    Adding tests for optional params

--- test/spec/params-applier/rules-applier.spec.js
+++ test/spec/params-applier/rules-applier.spec.js
@@ -19,4 +19,36 @@ describe('rules applier', () => {
 
 	});
 
+	it('replaces optional params in path by list rules', () => {
+
+		const path = '/path/:param-one(/:param-two)';
+		const rules = [
+			{ 'param-one': 1 },
+			{ 'param-one': 1, 'param-two': 2 },
+		];
+		const etalon = [
+			'/path/1',
+			'/path/1/2',
+		];
+
+		expect(applyRules(path, rules)).toHaveSameItems(etalon, true);
+
+	});
+
+	it('removes optional params in path by list rules whose value hasn\'t been provided', () => {
+
+		const path = '/path/:param-one(/:param-two)';
+		const rules = [
+			{},
+			{ 'param-one': 1 },
+		];
+		const etalon = [
+			'/path/:param-one',
+			'/path/1',
+		];
+
+		expect(applyRules(path, rules)).toHaveSameItems(etalon, true);
+
+	});
+
 });
